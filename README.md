c7adec77c4e3511f565d2654f2f2a8c62351066b
mohammadlua <mohammadluairir@gmail.com>
2017-06-25 12:59:49 -0700

    by mohammadlua v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# [darkplustm](https://telegram.me/darkplustm)
+# 
+#کد نصب هم بلدید دیگ خودتون
---
+++ auto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COUNTER=1
+while(true) do
+./dark.sh
+curl "https://api.telegram.org/bot[ToKeN]/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
+done
